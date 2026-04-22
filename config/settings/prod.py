d928439ed05b92ad3e7c9f0b29fd5e736194e398
Gedi <yiruubal@gmail.com>
2026-04-22 12:34:24 +0100

    fix: use CompressedStaticFilesStorage to fix missing manifest error

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -53,7 +53,7 @@ STORAGES = {
         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 
